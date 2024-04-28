38504187d9d463ae82f0eb5c74c84ef7bd83b2f1
KhenCahyo13 <khencahyo02@gmail.com>
2024-04-29 02:54:39 +0700

    ci: Config path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src/'),
+			pages: `${path.resolve(__dirname, './src/pages/')}`,
+			components: `${path.resolve(__dirname, './src/components/')}`,
+			types: `${path.resolve(__dirname, './src/types/')}`,
+      		themes: `${path.resolve(__dirname, './src/themes/')}`,
+			viewports: `${path.resolve(__dirname, './src/viewports/')}`,
+			assets: `${path.resolve(__dirname, './src/assets/')}`,
+		}
+	}
+});
